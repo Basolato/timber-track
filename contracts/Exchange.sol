a9a66e64d183a28b42c9875c1557b16eb567ab6d
Uli <alle@gmx-ist-cool.de>
2017-08-17 19:33:33 +0200

    added sol

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ import "./TokenTemplate.sol";
 
 contract Exchange {
     
+    
     //an easy version of an Exchange rate which only supports one input and one output
     struct ExchangeRate {
         uint inputToken;
@@ -32,7 +33,7 @@ contract Exchange {
     //Thats a function only called from the owner, that said the CA, as the Exchange should have access
     //To the exact same tokens
     function registerToken(address cAddress) returns(bool) {
-        if (msg.sender != owner) {
+        if (msg.sender == owner) {
             tokens.push(TokenTemplate(cAddress));
             return true;
         } else { return false; }
@@ -42,39 +43,44 @@ contract Exchange {
 
     /* transform a certain quantity of a material
     in a new one, according to the exchange rate */
-    //function transform(uint NrExchangeRate, uint quantity) {
-        //address user = msg.sender;
-        //ExchangeRate rate = rates[NrExchangeRate];
+    function transform(uint NrExchangeRate, uint quantity) returns(uint){
+        address user = msg.sender;
+        ExchangeRate storage rate = rates[NrExchangeRate];
         //get TokenAmount
-        //uint tokenAmount = tokens[rate.inputToken].getBalance(user);
+        uint tokenAmount = tokens[rate.inputToken].getBalance(user);
         
-        //We first have to check how much coins are available to convert
+        //calculate what is maximum able to transform
+        uint maxWithdrawal = tokenAmount / rate.inputFactor;
+    
+        //Define what he is actually to transform
+        uint actualWithdrawal = 0;
+
+        //Ensure that not more tokens are created than available from input token
+        if(maxWithdrawal >= quantity) {
+            actualWithdrawal = quantity;
+        } else {
+            actualWithdrawal = maxWithdrawal;
+        }
         
-
-
-        //uint rate;
-
-        //if ((iToken ==TREE_ID)&&(oToken==COAL_ID)) {
-        //    rate = rateTreeCoal;
-        //} else {
-        //    rate = rateTreePlank;
-        //}
-
-        // security checks
-        // @TODO
-
-        // delete the input token quantity
-        //owner = msg.sender;
-        //coins[iToken].modifyToken(owner, -quantity);
-
-        // create the output token
-        //uint oTokenQuantity = rate*quantity/100;
-        //coins[oToken].modifyToken(owner, oTokenQuantity);
-    //}
+        //Calculate actual Tokens that should be removed / added
+        uint tokenRemoved = actualWithdrawal * rate.inputFactor;
+        uint tokenCreated = actualWithdrawal * rate.outputFactor;
+        
+        tokens[rate.inputToken].modifyToken(user, -tokenRemoved);
+        tokens[rate.outputToken].modifyToken(user, tokenCreated);
+        
+        
+    }
 
     /* Only the central authority can modify the rates
     * @TODO
     * */
     function changeRate() {}
+    
+    function createRate(uint inputToken, uint inputFactor, uint outputToken, uint outputFactor)
+    {
+        ExchangeRate memory temp = ExchangeRate(inputToken, inputFactor, outputToken, outputFactor);
+        rates.push(temp);
+    }
 
 }
